cc94170a20049592f8492abf0338b7c9b065f160
Muhamad Hanif Zein <hanfimzein@Hanifs-MacBook-Pro.local>
2020-10-13 23:55:17 +0700

    revisi2

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,15 @@ const CACHE_NAME = "port-v8";
 var urlsToCache = [
     "/",
     "/nav.html",
+    "/manifest.json",
     "/index.html",
     "/pages/home.html",
     "/pages/track.html",
     "/pages/skill.html",
     "/pages/contact.html",
     "/img/pp.png",
-    "/img/icon.png",
+    "/img/192.png",
+    "/img/512.png",
     "/img/ppkapps.png",
     "/img/sukaderma.png",
     "/img/instruksi.jpg",
@@ -19,6 +21,10 @@ var urlsToCache = [
     "/js/materialize.min.js",
     "/js/nav.js",
     "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://cdn.materialdesignicons.com/5.6.55/fonts/materialdesignicons-webfont.woff2?v=5.6.55",
+    "https://cdn.materialdesignicons.com/5.6.55/fonts/materialdesignicons-webfont.woff?v=5.6.55",
+    "https://cdn.materialdesignicons.com/5.6.55/fonts/materialdesignicons-webfont.ttf?v=5.6.55",
+    "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
     "https://cdn.materialdesignicons.com/5.6.55/css/materialdesignicons.min.css"
 ];
 
